39d94738cfaa18e951c55da7d625b6ec72c33b63
Ernesto <ernesto.solares.guerrero@gmail.com>
2020-02-07 21:44:41 -0600

    added array example

---
+++ Assets/Intro Programming/Arrays.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    public int[] arreglo = new int[10];
+    // Start is called before the first frame update
+    void Start()
+    {
+        arreglo[0] = 1;
+        Debug.Log("arreglo[0]= " + arreglo[0]);
+    }
+
+}
