1cb728684e25df70312981c86260daf3a5fe7cb6
Ernesto <ernesto.solares.guerrero@gmail.com>
2020-02-26 17:45:31 -0600

    main menu added

---
+++ Assets/UI/GameLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoader : MonoBehaviour
+{
+    public void LoadScene(int sceneNumber)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneNumber);
+    }
+}
