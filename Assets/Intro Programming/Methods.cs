41b3fad8dece02d5106b61e241a524b0cc56b54c
Ernesto <ernesto.solares.guerrero@gmail.com>
2020-03-06 10:50:35 -0600

    repaso del examen

--- Assets/Intro Programming/Methods.cs
+++ Assets/Intro Programming/Methods.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Methods : MonoBehaviour
 {
 
     public int varA = 0;    // variable pública de tipo int (solo números enteros)
     public int varB = 0;    // variable pública de tipo int (solo números enteros)
-
+    public Text textoUI;
+    public InputField inputA;
+    public InputField inputB;
 
     void Start()
     {
@@ -17,5 +20,15 @@ public class Methods : MonoBehaviour
     public int Suma(int a, int b) // funcion llamada Suma de tipo int(enteros) que requiere 2 valores para ejecutarse (a y b) y regresa la suma de estos dos valores
     {
         return a + b;
+    }
+
+    public void SumaUI()
+    {
+        int varAInput = int.Parse(inputA.text);
+        int varBInput = int.Parse(inputB.text);
+
+        Debug.Log(Suma(varAInput, varBInput)); // imprime en consola
+
+        textoUI.text = Suma(varAInput, varBInput).ToString(); // cambia el texto UI
     }
 }
